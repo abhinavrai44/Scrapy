9165bc984c6025ec8c73e380a095b93ebecc93e8
Abhinav Rai <abhinavrai44@gmail.com>
2016-09-02 23:56:57 +0530

    Update dmoz_spider.py

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -4,7 +4,6 @@ import csv
 import pandas as pd
 import json
 from scrapy.linkextractors import LinkExtractor
-# from scrapy.contrib.linkextractors import LxmlLinkExtractor
 from scrapy.contrib.spiders import Rule, CrawlSpider
 
 class DmozSpider(scrapy.Spider):
@@ -28,12 +27,8 @@ class DmozSpider(scrapy.Spider):
                 self.all_links.append(link.url)
                 
                 for content in response.xpath('//meta[@name=\'keywords\']/@content').extract():
-                    # print "Content : ", content
-                    # f.write(content)
                     content = content.split(',')
-                    # print "C : "
                     for string in content:
-                        # print string
                         if string in self.meta_data.keys():
                             self.meta_data[string] += 1
                         else:
@@ -42,58 +37,10 @@ class DmozSpider(scrapy.Spider):
                             else:
                                 self.meta_data[string] = 1
 
-                    # print self.meta_data
-                    # words = []
-                    # flag = 0
-                    # for c in content:
-                    #     if(flag == 0):
-                    #         flag = 1
-                    #     else:
-                    #         if (c == ','):
-                    #             string = ''.join(words)
-
-                    #             if string in self.meta_data.keys():
-                    #                 self.meta_data[string] += 1
-                    #             else:
-                    #                 if (string==''):
-                    #                     continue
-                    #                 else:
-                    #                     self.meta_data[string] = 1
-                    #             words = [] 
-
-                    #         else:
-                    #             words.append(c)
+                    
                 yield scrapy.Request(link.url, callback=self.parse)
 
         # print self.count
         data = json.dumps(self.meta_data)
         file = open("output.json","w")
-        # print "abho"
         file.write(data)
-
-        # print self.meta_data
-
-        # Extract one webpage
-        # filename = str(self.count) + '.txt'
-
-        # with open(filename, 'wb') as f:
-        #   for content in response.xpath('//meta[@name=\'keywords\']/@content').extract():
-        #       print "Content : ", content
-  #             # f.write(content)
-  #             words = []
-  #             for c in content:
-  #                 if c==' ':
-  #                     continue
-  #                 if (c == ','):
-  #                     string = ''.join(words)
-
-  #                     if string in self.meta_data.keys():
-     #                      self.meta_data[string] += 1
-     #                  else:
-     #                      self.meta_data[string] = 1
-     #                  words = [] 
-
-  #                 else:
-  #                     words.append(c)
-        # print
-        # print self.meta_data
